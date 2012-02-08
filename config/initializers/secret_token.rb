4b33b267a4fb5d9b3111850874c40cfc986954da
Michael Boone <fuag155555@gmail.com>
2012-02-07 23:39:01 -0800

    initial ruby set-up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iguana::Application.config.secret_token = '4416fe18672b7d994dcd70496790b213838f6ef5fdb47a631ac1f120e29863d9ede7e2d18125c2acb828213f243df16d94662bd82f1d43a6c9ff39012a268596'
